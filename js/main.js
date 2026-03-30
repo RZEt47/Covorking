91a0fa070e7c05c95adcf821a727f14dd4fbbb44
Роман <121190511+RZEt47@users.noreply.github.com>
2026-03-30 23:21:10 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const slider = tns({
+    container: ".slider",
+
+    items: 2.7,
+    gutter: 10,
+    mouseDrag: true,
+    speed: 400,
+
+    autoWidth: true,
+    lazyload: true,
+    swipeAngle: false,
+
+    controlsContainer: ".locations__controls",
+    nav: false,
+});
+
+const prevMobile = document.querySelector(".locations__controls--mobile .prev");
+const nextMobile = document.querySelector(".locations__controls--mobile .next");
+
+prevMobile.onclick = function () {
+    slider.goTo("prev");
+};
+
+nextMobile.onclick = function () {
+    slider.goTo("next");
+};
---
+++ js/scrollToTop.js
@@ -0,0 +1,9 @@
+const scrollButton = document.querySelector("#scrollToTopBtn");
+
+window.addEventListener("scroll", function () {
+    if (window.pageYOffset > window.innerHeight) {
+        scrollButton.classList.add("top-link--visible");
+    } else {
+        scrollButton.classList.remove("top-link--visible");
+    }
+});
